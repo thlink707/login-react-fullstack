8862df3fc8c86138f569118604bbd66c745fa87b
thnlink707 <monsivais_th@outlook.com>
2026-03-14 16:24:52 -0600

    feature: Creación de rutas

---
+++ auth-back/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/singup', require('./routes/singup'));
+app.use('/api/user', require('./routes/user'));
+app.use('/api/singuot', require('./routes/singuot'));
+app.use('/api/todos', require('./routes/todos'));
+app.use('/api/refresh-token', require('./routes/refreshToken'));
+app.use('/api/login', require('./routes/login'));   
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ auth-back/routes/refreshToken.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.send("refresh token");
+});
+
+module.exports = router;
