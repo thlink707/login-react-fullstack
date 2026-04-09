f2d1f2246f0f720a491e74ff398ec8152a5f0ed2
thnlink707 <monsivais_th@outlook.com>
2026-04-09 16:17:46 -0600

    feature/Funciones para obtener el access y refresh token creadas

--- auth-front/src/auth/AuthProvider.tsx
+++ auth-front/src/auth/AuthProvider.tsx
@@ -1,18 +1,42 @@
 import { useContext, createContext, useState, useEffect, use } from "react"; 
-
+import type { AuthResponse } from "../types/types";
 interface AuthProviderProps {
   children: React.ReactNode;
 }
 
 const AuthContext = createContext({
   isAuthenticated: false,
+  getAccessToken: () => {},
+  saveUser: (userData: AuthResponse) => {},
+  getRefreshToken: () => {},
 });
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [accessToken, setAccessToken] = useState<string>("");
+  const [refreshToken, setRefreshToken] = useState<string>("");
 
+  function getAccessToken() {
+    return accessToken;
+  }
+
+  function getRefreshToken() {
+    const token = localStorage.getItem("token");
+    if (token){
+      const { refreshToken } = JSON.parse(token);
+      return refreshToken;
+    }
+  }
+
+  function saveUser(userData: AuthResponse) {
+    setAccessToken(userData.body.accessToken);
+    setRefreshToken(userData.body.refreshToken);
+
+    localStorage.setItem("token", JSON.stringify(userData.body.refreshToken));
+    setIsAuthenticated(true);
+  }
   return (
-    <AuthContext.Provider value={{ isAuthenticated }}>
+    <AuthContext.Provider value={{ isAuthenticated, getAccessToken, saveUser, getRefreshToken }}>
       {children}
     </AuthContext.Provider>
   );
